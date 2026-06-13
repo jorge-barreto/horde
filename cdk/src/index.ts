51658f4fde2dfff122afe135000fbf21dd4c0c46
Jorge Barreto <jrbd93@gmail.com>
2026-06-12 20:56:30 -0400

    feat(cdk): add networkMode prop and HordeNetworkMode type

--- cdk/src/index.ts
+++ cdk/src/index.ts
@@ -1,2 +1,2 @@
 export { HordeWorker, WORKER_CONTAINER_NAME } from "./horde-worker";
-export type { HordeWorkerProps, HordeWorkerSecrets } from "./horde-worker-props";
+export type { HordeNetworkMode, HordeWorkerProps, HordeWorkerSecrets } from "./horde-worker-props";
